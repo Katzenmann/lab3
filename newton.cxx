b2e15870c6c3fe8f592e58492d02bcc3a15b8d35
Hani Awwad <haawwa@pc03.pool.tp1.uni-duesseldorf.de>
2015-11-10 16:37:07 +0100

    tag

--- newton.cxx
+++ newton.cxx
@@ -1,13 +1,29 @@
 #include <iostream>
 #include <complex>
+#include <cmath>
 
+using namespace std;
 int main(){
 
-  // Example how to declare a single complex variable:
-  complex<double> c;
-
-  // d = 1 + 2i 
-  complex<double> d = complex<double>(1.0, 2.0); 
-
+  int N=40;
+  complex<double> znull;
+  complex<double> zeins,zsave;
+  double bedingung=1e-8;
+  //z0=complex<double>(1.0,1.0);
+  for(double re=-2; re<=2; re+=0.01){
+    for(double im=-2; im<=2; im+=0.01){
+    znull = complex<double> (re,im);
+    zsave = znull;
+    int i;
+      for(i=1; i<=N; i++){
+      zeins=znull-(znull*znull*znull-1.0)/(3.0*znull*znull);
+      if(abs(znull-zeins)<=bedingung){
+	break;
+      }
+      znull=zeins;
+      }
+    cout<<real(zsave)<<"\t"<<imag(zsave)<<"\t"<<i<<endl;
+    }
+  }
   return 0;
 }
