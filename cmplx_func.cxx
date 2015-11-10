e1e380bdaee75a13f20ac53d98807a9513214c90
Hani Awwad <haawwa@pc03.pool.tp1.uni-duesseldorf.de>
2015-11-10 15:55:02 +0100

    nachricht

--- cmplx_func.cxx
+++ cmplx_func.cxx
@@ -1,13 +1,35 @@
 #include <iostream>
 #include <complex>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+
+complex<double> katze(double);
 
 int main(){
+  ofstream out("complexedaten.dat");
+  int N=200;
+  double dphi=2*M_PI/N, phi;
+  complex<double> fc[N];
+  for(int i=1; i<=N; i++){
+    
+    phi   = i*dphi-M_PI;
+    fc[i] = katze(phi);  
+  }
+  for(int i=1; i<=N; i++){
+  
+  out<< i*dphi-M_PI << " \t"<<real(fc[i])<< " \t"<<imag(fc[i])<<" \t"<<abs(fc[i])<<endl;
 
-  // Example how to declare a single complex variable:
-  complex<double> c;
-
-  // d = 1 + 2i 
-  complex<double> d = complex<double>(1.0, 2.0); 
-
+  cout<< i*dphi-M_PI << " \t"<<real(fc[i])<< " \t"<<imag(fc[i])<<" \t"<<abs(fc[i])<<endl;   
+  }  
+  out.close(); 
+  
+  
   return 0;
+}
+
+complex<double> katze(double phi){
+  complex<double> ii = complex<double>(0.0,1.0);
+  return tan(phi*(1.0+ii));
 }
